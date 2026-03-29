ea7efd35e2b529f73328c36913577b94a1284bc1
leonardo araujo <leoaraujofnd@gmail.com>
2026-03-29 19:14:14 -0300

    feat: Redirect home route to dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
