99f5416e4f3eb0f146ab015a0885e0b28d0d9fb2
leonardo araujo <leoaraujofnd@gmail.com>
2026-03-29 19:24:33 -0300

    feat: Seed test data for user, company, and project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Project;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $company = Company::factory()->create([
+            'name' => 'Test Company',
+            'corporate_name' => 'Test Company LTDA',
+            'user_id' => $user->id,
+            'email' => 'test@company.com',
+        ]);
+
+        $project = Project::factory()->create([
+            'name' => 'Test Project',
+            'company_id' => $company->id,
+            'manager_id' => $user->id,
         ]);
     }
 }
